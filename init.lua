b0e8a1ab76bd67a4b8f710655b6a4a92c4b73384
posaidon09 <halohalo090909@gmail.com>
2024-07-07 12:20:27 +0100

    testing again

--- init.lua
+++ init.lua
@@ -473,4 +473,26 @@ require("themery").setup({
   themes = {"gruvbox-material", "rose-pine", "nordic"}, -- Your list of installed colorschemes
   livePreview = true, -- Apply theme while browsing. Default to true.
 })
-require("neocord").setup()
+require("neocord").setup({
+    -- General options
+    logo                = "https://cdn.discordapp.com/attachments/1257471054365065287/1258861437208756384/nekovim-the-twost.png?ex=668b8fd3&is=668a3e53&hm=8e48b04e4af6625864bb70faf85f86d031c8bf12ab685221b16a61eba932c47f&",                     -- "auto" or url
+    logo_tooltip        = nil,                        -- nil or string
+    main_image          = "language",                 -- "language" or "logo"
+    client_id           = "1157438221865717891",      -- Use your own Discord application client id (not recommended)
+    log_level           = nil,                        -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
+    debounce_timeout    = 10,                         -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
+    blacklist           = {},                         -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
+    file_assets         = {},                         -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
+    show_time           = true,                       -- Show the timer
+    global_timer        = false,                      -- if set true, timer won't update when any event are triggered
+
+    -- Rich Presence text options
+    editing_text        = "Editing %s",               -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
+    file_explorer_text  = "Browsing %s",              -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
+    git_commit_text     = "Committing changes",       -- Format string rendered when committing changes in git (either string or function(filename: string): string)
+    plugin_manager_text = "Managing plugins",         -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
+    reading_text        = "Reading %s",               -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
+    workspace_text      = "Working on %s",            -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
+    line_number_text    = "Line %s out of %s",        -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
+    terminal_text       = "Using Terminal",           -- Format string rendered when in terminal mode.
+})
