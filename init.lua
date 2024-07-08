6d4d5406ce2adae62dc8a8a1360ffbe94e4c35cb
tungstengmd <gostdud@gmail.com>
2024-07-08 16:27:58 +0100

    3rd org

--- init.lua
+++ init.lua
@@ -23,25 +23,12 @@ require("lazy").setup(
                   priority = 1000,
                   config = true,
               },
-  {
+  	      {
     "nvim-neorg/neorg",
-    dependencies = { "luarocks.nvim" },
-    version = "*",
-    config = function()
-      require("neorg").setup {
-        load = {
-          ["core.defaults"] = {},
-          ["core.concealer"] = {},
-          ["core.dirman"] = {
-            config = {
-              workspaces = {
-                notes = "~/notes",
-              },
-              default_workspace = "notes",
-            },
-          },
-        },
-      },
+    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = "*", -- Pin Neorg to the latest stable release
+    config = true,
+},
             {
                 "nvim-orgmode/orgmode",
                 event = "VeryLazy",
