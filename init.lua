95426b3bd6eb14b12784d3a79b385f6424d85239
tungstengmd <gostdud@gmail.com>
2024-07-22 20:17:55 +0100

    we got diagnostic icons!

--- init.lua
+++ init.lua
@@ -406,7 +406,9 @@ vim.diagnostic.config(
         signs = {
             text = {
                 [vim.diagnostic.severity.ERROR] = "󰅚",
-                [vim.diagnostic.severity.WARN] = "󰀪"
+                [vim.diagnostic.severity.WARN] = "󰀪",
+                [vim.diagnostic.severity.HINT] = "",
+                [vim.diagnostic.severity.INFO] = "",
             },
             linehl = {
                 [vim.diagnostic.severity.ERROR] = "ErrorMsg"
