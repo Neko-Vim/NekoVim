c63048d3bd99d3ab06b14436ad40f7fee6c2dd24
tungstengmd <gostdud@gmail.com>
2024-07-30 20:33:05 +0100

    mmhhhh greetingssssssssssss

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,3 +1,28 @@
+local function getGreeting()
+  local tableTime = os.date("*t")
+  local hour = tableTime.hour
+  local greetingsTable = {
+    [1] = "  Sleep well!",
+    [2] = "  Good morning!",
+    [3] = "  Good afternoon!",
+    [4] = "  Good evening!",
+    [5] = "望 Good night!",
+  }
+  local greetingIndex = 0
+  if hour == 23 or hour < 7 then
+    greetingIndex = 1
+  elseif hour < 12 then
+    greetingIndex = 2
+  elseif hour >= 12 and hour < 18 then
+    greetingIndex = 3
+  elseif hour >= 18 and hour < 21 then
+    greetingIndex = 4
+  elseif hour >= 21 then
+    greetingIndex = 5
+  end
+  return greetingsTable[greetingIndex]
+end
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -31,6 +56,7 @@ require("lazy").setup({
     notify = false,
   },
 })
+vim.notify = require("notify")(getGreeting(), "info", {title = "Greetings", stages = "slide", render = "minimal"})
 require("mason").setup(
     {
         ui = {border = {"╭", "─", "╮", "│", "╯", "─", "╰", "│"}}
