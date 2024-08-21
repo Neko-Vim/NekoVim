dc8a9702f35af490235d4617070ccdb395e81508
harley <gostdud@gmail.com>
2024-08-21 15:54:52 +0100

    look

--- lua/nekovim/configs/other.lua
+++ lua/nekovim/configs/other.lua
@@ -22,43 +22,6 @@ local function getGreeting()
     end
     return greetingsTable[greetingIndex]
 end
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
-            { "\nPress any key to exit..." },
-            }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-    end
-end
-vim.opt.rtp:prepend(lazypath)
-
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
-require("lazy").setup({
-    spec = {
-        { import = "plugins" },
-    },
-    news = {
-        neovim = true,
-    },
-    defaults = {
-        lazy = false,
-        version = false,
-    },
-    install = { colorscheme = { "default" } },
-    checker = {
-        enabled = true,
-        notify = false,
-    },
-})
 vim.notify = require("notify")(getGreeting(), "info", {title = "Greetings", stages = "slide", render = "minimal"})
 vim.opt.termguicolors = true
 require("monokai-pro").setup()
